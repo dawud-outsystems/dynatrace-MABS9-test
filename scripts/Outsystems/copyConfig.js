53caa013ebd1833a0eaffa3aa02837b3fd4f9182
dawud-outsystems <dawud.mendez@outsystems.com>
2023-03-06 14:21:43 +0100

    Update copyConfig.js

--- scripts/Outsystems/copyConfig.js
+++ scripts/Outsystems/copyConfig.js
@@ -18,7 +18,7 @@ module.exports = function (context) {
         path  = context.requireCordovaModule("path");
     }
 
-    var wwwPath = path.join(context.opts.projectRoot,"DynaTest");
+    var wwwPath = path.join(context.opts,"DynaTest");
     var configPath = path.join(wwwPath, "dynatraceConfig");
     files = fs.readdirSync(configPath);
     if(files.length >0){
